1157a9ebdef58c1689a2b142fea6bd8ba9f5315f
demandsssss <65413674+demandsssss@users.noreply.github.com>
2023-01-01 17:45:59 +0000

    day1

---
+++ day1.py
@@ -0,0 +1,17 @@
+# Open the file in read-only mode
+
+curr = 0 
+maxi = 0 
+
+
+with open('day1_input.txt', 'r') as file:
+    # Read the contents of the file
+    lines = file.readlines()
+    print(lines)
+for line in lines:
+    if line != '\n' :
+        curr = curr+ int(line)
+    else:
+        maxi = max(maxi,curr)
+        curr = 0
+print(maxi)
---
+++ day1_2.py
@@ -0,0 +1,23 @@
+# Open the file in read-only mode
+import heapq
+curr = 0 
+maxi = 0 
+asshold = []
+
+
+with open('day1_input.txt', 'r') as file:
+    # Read the contents of the file
+    lines = file.readlines()
+    print(lines)
+for line in lines:
+    if line != '\n' :
+        curr = curr+ int(line)
+    else:
+        asshold.append(curr)
+        curr = 0
+heap = [-x for x in asshold]
+heapq.heapify(heap)
+
+for _ in range(3):
+    maxi = maxi + -heapq.heappop(heap)
+print(maxi)
