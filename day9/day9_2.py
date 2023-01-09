8da36cc73bceb3089d929e7f7d3753157fa2689b
demandsssss <65413674+demandsssss@users.noreply.github.com>
2023-01-09 00:43:53 +0000

    correct answer

--- day9/day9_2.py
+++ day9/day9_2.py
@@ -1,11 +1,47 @@
+def movement(rope,H, T):
+    # no need to see left right , the main point is to follow prev node and do adjustment
+    
+    if abs(H[0] - T[0])>1 and abs(H[1] - T[1]) >1: # move  2 ways 
+        
+        if H[1] > T[1]:
+            T= (T[0] ,T[1]+1)
+        else:
+            T= (T[0] ,T[1]-1)
+
+        if H[0] > T[0]:
+            T= (T[0]+1 ,T[1])
+        else:
+            T= (T[0]-1 ,T[1])
+
+    elif abs(H[1] - T[1]) >1:# 'move left / right only '
+        ####### this is very important :use H[0] as the ref as the anchor, instead of T[0]
+        ### e.g. H(2,5) , T(0,4)
+       
+        if H[1] > T[1]:
+            T= (H[0] ,T[1]+1)
+        else:
+            T= (H[0] ,T[1]-1)
+        # not in same row, need to check if they are diagonal if 'l''r'
+        # move to same row with H 
+    elif abs(H[0] - T[0]) >1:# 'move up down only '
+        ####### this is very important :use H[0] as the ref as the anchor, instead of T[0]
+        ### e.g. H(2,5) , T(0,4)
+        if H[0] > T[0]:
+            T= (T[0]+1 ,H[1])
+        else:
+            T= (T[0]-1 ,H[1])
+        # not in same row, need to check if they are diagonal if 'l''r'
+        # move to same row with H 
+    print(rope, H,T)
+    return T
 import re
 from collections import deque
-with open('day9/day9_sample1.txt', 'r') as file:
+with open('day9/day9_input.txt', 'r') as file:
     # Read the contents of the file
     lines = file.readlines()
 ropes = [(0,0) for i in range(10)]
 
-adj=''
+
 
 
 H = (0,0)
@@ -20,69 +56,9 @@ for line in lines:
     step = int(step)
     for i in range(step):
         ropes[0] = (ropes[0][0] + MOVEROW[dir], ropes[0][1] + MOVECOL[dir])
-        
-        for rope in range(len(ropes)-1):
-           # print('error before', ropes[rope] , '   ', ropes[rope+1])
-
-            
-            # T is some row as H and it , just follow H  , or it means moving L / R
-            if ropes[rope+1][0] == ropes[rope][0] and abs(ropes[rope][1] - ropes[rope+1][1]) >1:
-                ropes[rope+1]= (ropes[rope+1][0]+ MOVEROW[dir],ropes[rope+1][1]+MOVECOL[dir])
-            # not in same row, need to check if they are diagonal if 'l''r'
-            # move to same row with H 
-            elif ropes[rope+1][0] != ropes[rope][0] and abs(ropes[rope][1] - ropes[rope+1][1]) >1 and (dir == 'L' or dir == 'R' ):
-               
-                ropes[rope+1]=(ropes[rope][0], ropes[rope+1][1]+MOVECOL[dir] )
-        
-            elif ropes[rope+1][1] == ropes[rope][1] and abs(ropes[rope][0] - ropes[rope+1][0]) >1:
-                ropes[rope+1]= (ropes[rope+1][0]+ MOVEROW[dir],ropes[rope+1][1]+MOVECOL[dir])
-            elif ropes[rope+1][1] != ropes[rope][1] and abs(ropes[rope][0] - ropes[rope+1][0]) >1 and (dir == 'U' or dir == 'D' ):
-                ropes[rope+1]=(ropes[rope+1][0]+ MOVEROW[dir] , ropes[rope][1])
-            elif ropes[rope+1][0] == ropes[rope][0] and abs(ropes[rope][1] - ropes[rope+1][1]) <=1:
-                 a = 1 
-            elif ropes[rope+1][1] == ropes[rope][1] and abs(ropes[rope][0] - ropes[rope+1][0]) <=1:
-                a=1
-                #print('here: same row , same place  :',rope+1) 
-            elif ropes[rope+1][0] != ropes[rope][0]  and ropes[rope+1][1] != ropes[rope][1] and abs(ropes[rope][1] - ropes[rope+1][1]) >1 and abs(ropes[rope][0] - ropes[rope+1][0]) >1 :
-                print('all side need to update')
-                #ropes[rope+1]=(ropes[rope][0]+ MOVEROW[dir], ropes[rope][1])
-            elif ropes[rope+1][1] != ropes[rope][1] and abs(ropes[rope][1] - ropes[rope+1][1]) >1 and (dir == 'U' or dir == 'D' ):
-                # move up but actually need to move 2 dir
-                
-                H=(ropes[rope+1][0]+ MOVEROW[dir] , ropes[rope+1][1]+MOVECOL[dir])
-                #print('1 :' , H)
-                if (dir == 'U' or dir == 'D') and H[1] -T[1] >0:
-                    adj = 'R'
-                elif (dir == 'U' or dir == 'D') and H[1] -T[1] <0:
-                    adj = 'L'
-                elif(dir == 'L' or dir == 'R') and H[0] -T[0] >0:
-                    adj = 'U'
-                elif(dir == 'L' or dir == 'R') and H[0] -T[0] <0:
-                    adj = 'D'
-                H = (H[0]+ MOVEROW[adj] , H[1]+MOVECOL[adj])
-                #print ('2: ',H)
-                ropes[rope+1] = H
-            elif ropes[rope+1][0] != ropes[rope][0] and abs(ropes[rope][0] - ropes[rope+1][0]) >1 and (dir == 'L' or dir == 'R' ):
-                # move up but actually need to move 2 dir
-                print( rope,'before node:',ropes[rope], '  node + 1:' , ropes[rope+1])
-                H=(ropes[rope+1][0]+ MOVEROW[dir] , ropes[rope+1][1]+MOVECOL[dir])
-                print('before node:',ropes[rope])
-                print('1 :' , H)
-                if (dir == 'U' or dir == 'D') and H[1] -T[1] >0:
-                    adj = 'R'
-                elif (dir == 'U' or dir == 'D') and H[1] -T[1] <0:
-                    adj = 'L'
-                elif(dir == 'L' or dir == 'R') and H[0] -T[0] >0:
-                    adj = 'U'
-                elif(dir == 'L' or dir == 'R') and H[0] -T[0] <0:
-                    adj = 'D'
-                H = (H[0]+ MOVEROW[adj] , H[1]+MOVECOL[adj])
-                print ('2: ',H)
-                ropes[rope+1] = H
-            else:
-                print('here',rope+1, ropes[rope+1][1] != ropes[rope][1], '   ',abs(ropes[rope][0] - ropes[rope+1][0]) >1  ,'  ', (dir == 'U' or dir == 'D' ))
-            #print('error after', ropes[rope] , '   ', ropes[rope+1])
-       
+        ropes[1] = movement(1,ropes[0],ropes[1])
+        for rope in range(2,len(ropes)):
+            ropes[rope] = movement(rope,ropes[rope-1], ropes[rope])
     
         if ropes[9] not in path:
             print('adding ropes[9]:',ropes[9] )
